068e08f7a1ffd66918e4417a4de82c72a8ac5dc6
Iris Fang <iris.fang@nelly-solutions.com>
2022-12-04 06:23:09 +0100

    Format day4

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -3,32 +3,29 @@ import { readByLine, sumArray } from "../utils";
 const parseData = (data: string[]) => {
   return data.map((line) => {
     const pair = line.split(",");
-    return pair.map(range => range.split("-").map((i) => parseInt(i))).flat()
+    return pair.map((range) => range.split("-").map((i) => parseInt(i))).flat();
   });
 };
 
 const isFullyOverlap = (pair: number[]) => {
-  const [a, b, c, d] = pair
-  return ((a >= c && b <= d) || (a <= c && b >= d)) ? 1 : 0
-}
+  const [a, b, c, d] = pair;
+  return (a >= c && b <= d) || (a <= c && b >= d) ? 1 : 0;
+};
 
 const isOverlap = (pair: number[]) => {
-  const [a, b, c, d] = pair
-  return (b < c || a > d) ? 0 : 1
-}
+  const [a, b, c, d] = pair;
+  return b < c || a > d ? 0 : 1;
+};
 
 export const solution = (data: string[]) => {
   const parsedData = parseData(data);
 
-  const res1 = sumArray(parsedData.map(pair => isFullyOverlap(pair)));
+  const res1 = sumArray(parsedData.map((pair) => isFullyOverlap(pair)));
   console.log(">>> Part 1:", res1);
 
-  const res2 = sumArray(parsedData.map(pair => isOverlap(pair)));
-  console.log('>>> Part 2:', res2);
-  return [
-    res1,
-    res2,
-  ];
+  const res2 = sumArray(parsedData.map((pair) => isOverlap(pair)));
+  console.log(">>> Part 2:", res2);
+  return [res1, res2];
 };
 
 const data = readByLine("./day04/data");
